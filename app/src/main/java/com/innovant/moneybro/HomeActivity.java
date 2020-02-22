1a0e506e0fec20d10f247d4612b6ac366cc21cf6
Fabio Flores <ernesto.flores@aplaceformom.com>
2020-02-22 10:46:41 -0600

    show transactions on home page

--- app/src/main/java/com/innovant/moneybro/HomeActivity.java
+++ app/src/main/java/com/innovant/moneybro/HomeActivity.java
@@ -2,17 +2,26 @@ package com.innovant.moneybro;
 
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.util.Log;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.Timestamp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import androidx.drawerlayout.widget.DrawerLayout;
 
@@ -21,10 +30,25 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.view.Menu;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 public class HomeActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
+    private ListView listView;
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +68,35 @@ public class HomeActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        listView = findViewById(R.id.transactions_list);
+        mAuth = FirebaseAuth.getInstance();
+
+        String uid = mAuth.getUid();
+        db = FirebaseFirestore.getInstance();
+        db.collection("transactions").whereEqualTo("ownerId", uid).get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            List<Map<String, Object>> transacciones = new ArrayList<>();
+                            for(QueryDocumentSnapshot doc : task.getResult()) {
+                                Map<String, Object> transaccion = doc.getData();
+                                transacciones.add(transaccion);
+                            }
+                            CustomAdapter customAdapter = new CustomAdapter(transacciones);
+                            listView.setAdapter(customAdapter);
+                        }
+                    }
+                });
+
+//        ListView listView = findViewById(R.id.transactions_list);
+//        ArrayList<String> arrayList = new ArrayList<>();
+//        arrayList.add("android");
+//        arrayList.add("ios");
+//
+//        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
+//        listView.setAdapter(arrayAdapter);
     }
 
     @Override
@@ -62,5 +115,47 @@ public class HomeActivity extends AppCompatActivity {
 
     public void startTransaction(View view) {
         startActivity(new Intent(HomeActivity.this, TransactionActivity.class));
+    }
+
+    class CustomAdapter extends BaseAdapter {
+        private List<Map<String, Object>> transacciones;
+
+        public CustomAdapter(List<Map<String, Object>> transacciones) {
+            this.transacciones = transacciones;
+        }
+        @Override
+        public int getCount() {
+            return transacciones.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            view = getLayoutInflater().inflate(R.layout.transaction_list_row, null, false);
+            TextView title = view.findViewById(R.id.tListTitle);
+            TextView type = view.findViewById(R.id.tListType);
+            TextView deadline = view.findViewById(R.id.tListDeadline);
+            TextView amount = view.findViewById(R.id.tListAmount);
+
+            Timestamp timestamp = (Timestamp) transacciones.get(i).get("deadline");
+            Date fecha = timestamp.toDate();
+            Locale locale = new Locale("es", "ES");
+            DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.DEFAULT, locale);
+
+            title.setText(transacciones.get(i).get("userName").toString());
+            type.setText(transacciones.get(i).get("type").toString());
+            deadline.setText("Finaliza: " + dateFormat.format(fecha));
+            amount.setText("₡" + transacciones.get(i).get("amount").toString());
+            return view;
+        }
     }
 }
--- app/src/main/java/com/innovant/moneybro/ui/home/HomeFragment.java
+++ app/src/main/java/com/innovant/moneybro/ui/home/HomeFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -13,6 +15,8 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.innovant.moneybro.R;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
 
