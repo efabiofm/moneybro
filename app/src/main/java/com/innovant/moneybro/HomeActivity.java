6c96c167984aeafafe9827be5c4ed2b5cfb68ad6
Fabio Flores <ernesto.flores@aplaceformom.com>
2020-03-09 11:03:39 -0600

    create user profile

--- app/src/main/java/com/innovant/moneybro/HomeActivity.java
+++ app/src/main/java/com/innovant/moneybro/HomeActivity.java
@@ -3,15 +3,21 @@ package com.innovant.moneybro;
 import android.content.Context;
 import android.os.Bundle;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -24,51 +30,40 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ListView listView;
     private FirebaseAuth mAuth;
     private FirebaseFirestore db;
     private TextView noData;
+    private TextView navHeaderUsername;
+    private TextView navHeaderEmail;
     private ProgressBar progressBar;
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+
         noData = findViewById(R.id.noDataText);
         noData.setVisibility(View.GONE);
-
         progressBar = findViewById(R.id.progressBarHome);
+        listView = findViewById(R.id.transactions_list);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
-                .setDrawerLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
         final Context context = this;
 
-        listView = findViewById(R.id.transactions_list);
-        mAuth = FirebaseAuth.getInstance();
-
-        String uid = mAuth.getUid();
-        db = FirebaseFirestore.getInstance();
-        db.collection("transactions").whereArrayContains("showTo", uid).get()
+        db.collection("transactions").whereArrayContains("showTo", mAuth.getUid()).get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
@@ -98,6 +93,8 @@ public class HomeActivity extends AppCompatActivity {
                         }
                     }
                 });
+
+        setupNavDrawer();
     }
 
     @Override
@@ -133,5 +130,56 @@ public class HomeActivity extends AppCompatActivity {
         details.putExtra("amount", "" + Utils.calcInterest(monto, interes));
         details.putExtra("category", transaction.get("category").toString());
         return details;
+    }
+
+    private void setupNavDrawer() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
+                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+        String uEmail = mAuth.getCurrentUser().getEmail();
+        View navHeaderView= navigationView.getHeaderView(0);
+        navHeaderUsername = navHeaderView.findViewById(R.id.navHeaderUsername);
+        navHeaderEmail = navHeaderView.findViewById(R.id.navHeaderEmail);
+        navHeaderEmail.setText(uEmail);
+
+        db.collection("users").document(mAuth.getUid()).get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        String username = documentSnapshot.get("name").toString();
+                        navHeaderUsername.setText(username);
+                    }
+                });
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        drawer.closeDrawers();
+        switch (id) {
+            case R.id.nav_profile:
+                startActivity(new Intent(HomeActivity.this, ProfileActivity.class));
+                break;
+            case R.id.nav_logout:
+                mAuth.signOut();
+                Toast.makeText(HomeActivity.this, "Sesión finalizada", Toast.LENGTH_LONG).show();
+                startActivity(new Intent(HomeActivity.this, MainActivity.class));
+                break;
+        }
+        return false;
     }
 }
